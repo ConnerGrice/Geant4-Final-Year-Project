b455e8bc14ed8f3ed9f0cd0257c70fb702902890
Conner Grice <connergrice12@gmail.com>
2021-11-12 19:19:19 +0100

    Moving Build folder to project directory

---
+++ build/CMakeFiles/gold.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/local1/eclipse-workspace/gold/gold.cpp" "/home/local1/eclipse-workspace/gold/build/CMakeFiles/gold.dir/gold.cpp.o"
+  "/home/local1/eclipse-workspace/gold/src/GoldActionInitialization.cpp" "/home/local1/eclipse-workspace/gold/build/CMakeFiles/gold.dir/src/GoldActionInitialization.cpp.o"
+  "/home/local1/eclipse-workspace/gold/src/GoldDetectorConstruction.cpp" "/home/local1/eclipse-workspace/gold/build/CMakeFiles/gold.dir/src/GoldDetectorConstruction.cpp.o"
+  "/home/local1/eclipse-workspace/gold/src/GoldPhysicsList.cpp" "/home/local1/eclipse-workspace/gold/build/CMakeFiles/gold.dir/src/GoldPhysicsList.cpp.o"
+  "/home/local1/eclipse-workspace/gold/src/GoldPrimaryGeneratorAction.cpp" "/home/local1/eclipse-workspace/gold/build/CMakeFiles/gold.dir/src/GoldPrimaryGeneratorAction.cpp.o"
+  "/home/local1/eclipse-workspace/gold/src/GoldRunAction.cpp" "/home/local1/eclipse-workspace/gold/build/CMakeFiles/gold.dir/src/GoldRunAction.cpp.o"
+  "/home/local1/eclipse-workspace/gold/src/GoldSensitiveDetector.cpp" "/home/local1/eclipse-workspace/gold/build/CMakeFiles/gold.dir/src/GoldSensitiveDetector.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/clhep/2.4.4.0/lib/CLHEP-2.4.4.0/../../include"
+  "../include"
+  "/usr/local/geant4.10.07.p02/include/Geant4"
+  "/usr/local/clhep/2.4.4.0/include"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib64/qt5/mkspecs/linux-g++"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtWidgets"
+  "/usr/include/qt5/QtOpenGL"
+  "/usr/include/qt5/QtPrintSupport"
+  "/usr/local/XercesC/3.2.3/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/gold.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gold.dir/gold.cpp.o"
+  "CMakeFiles/gold.dir/src/GoldActionInitialization.cpp.o"
+  "CMakeFiles/gold.dir/src/GoldDetectorConstruction.cpp.o"
+  "CMakeFiles/gold.dir/src/GoldPhysicsList.cpp.o"
+  "CMakeFiles/gold.dir/src/GoldPrimaryGeneratorAction.cpp.o"
+  "CMakeFiles/gold.dir/src/GoldRunAction.cpp.o"
+  "CMakeFiles/gold.dir/src/GoldSensitiveDetector.cpp.o"
+  "gold"
+  "gold.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gold.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
